7fb956f4cb37b8802d13e8c2a76be059e169f270
Xhylan <mbdollentes@up.edu.ph>
2024-02-15 22:05:32 +0800

    added more functions and a to-do on the python code

--- python/lab01.py
+++ python/lab01.py
@@ -1,8 +1,67 @@
+import random
+
+
+def init_matrix(n, min, max):
+    matrix = []
+
+    for i in range(0, n):
+        row = []
+        for j in range(0, n):
+            row.append(random.randint(min, max))
+        matrix.append(row)
+
+    return matrix
+
+
+def init_vector(n, min, max):
+    vector = []
+
+    for i in range(0, n):
+        vector.append(random.randint(min, max))
+
+    return vector
+
+
+def init_empty_vector(n):
+    vector = []
+
+    for i in range(0, n):
+        vector.append(0)
+
+    return vector
+
+
+def print_matrix(n, matrix):
+    for i in range(0, n):
+        for j in range(0, n):
+            print(str(matrix[i][j]), end="\t")
+        print("\n")
+
+#
+# TODO: Complete the following function.
+# Use lab01.c as your guide.
+
+
+def pearson_cor(matrix, vector, size):
+    rho_vector = init_empty_vector(size)
+
+    return rho_vector
+
+
 def main():
     n = 0
 
     while (n < 1):
         n = int(input("Please enter n: "))
+
+    matrix = init_matrix(n, 1, 10)
+    print_matrix(n, matrix)
+    vector = init_vector(n, 1, 10)
+
+    pearson_cor(matrix, vector, n)
+
+    matrix = None
+    vector = None
 
 
 if __name__ == "__main__":
