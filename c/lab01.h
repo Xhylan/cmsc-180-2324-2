2a58ad5d0fcbe4e9d90a95b9a04951e70048162b
Xhylan <mbdollentes@up.edu.ph>
2024-02-16 18:41:16 +0800

    removed summation functions

--- c/lab01.h
+++ c/lab01.h
@@ -15,45 +15,14 @@ int ** init_matrix(int n, int min, int max){
     return matrix;
 }
 
-int * init_empty_vector(int n){
-    return (int *) malloc(sizeof(int) * n);
-}
-
 int * init_vector(int n, int min, int max){
-    int * vector = init_empty_vector(n);
+    int * vector = (int *) malloc(n * sizeof(int));
 
     for(int i = 0; i < n; i++){
       vector[i] = (rand() % (max - min)) + min;
     }
 
     return vector;
-}
-
-int get_vector_sum(int *vector, int n){
-  int sum = 0;
-
-  for (int i = 0; i < n; i++)
-    sum = sum + vector[i];
-
-  return sum;
-}
-
-int get_vector_sum_sq(int *vector, int n){
-  int sum = 0;
-
-  for (int i = 0; i < n; i++)
-    sum = sum + (vector[i] * vector[i]);
-
-  return sum;
-}
-
-int get_sum_of_products(int *vectorA, int *vectorB, int n){
-  int sum = 0;
-
-  for (int i = 0; i < n; i++)
-    sum = sum + (vectorA[i] * vectorB[i]);
-
-  return sum;
 }
 
 int * get_vector_from_column(int ** matrix, int n, int col){
