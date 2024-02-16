865459f61c6303e1ba2c68c9512837be32169b54
Xhylan <mbdollentes@up.edu.ph>
2024-02-16 10:53:33 +0800

    added code computing for the pearson correlation coefficient. needs polishing.

--- c/lab01.c
+++ c/lab01.c
@@ -2,20 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <math.h>
 #include "lab01.h"
 
-int * pearson_cor(int ** matrix, int * vector, int n);
+double * pearson_cor(int ** matrix, int * vector, int n);
 
 int main(int argc, char *argv[]){
-  int ** matrix = NULL, * vector = NULL, * rho_vector = NULL;
+  int ** matrix = NULL, * vector = NULL, n = 0;
+  double * rho_vector = NULL;
   //clock_t start, end;
 
   if (argc < 2) {
-    printf("Usage: ./lab01 [n]");
-    return EXIT_FAILURE;
+    printf("Please enter n (n > 0): ");
+    while(n < 1){
+      scanf("%d", &n);
+      getchar();
+    }
   }
-  
-  int n = (int) *argv[1];
+ 
+  if (argc == 2) {
+    int n = (int) *argv[1];
+  }
+
   srand(time(NULL));
  
   matrix = init_matrix(n, 1, 10);
@@ -28,9 +36,13 @@ int main(int argc, char *argv[]){
     return EXIT_FAILURE;
   }
 
-  
-  if (pearson_cor(matrix, vector, n) == NULL){
+  rho_vector = pearson_cor(matrix, vector, n);
+  if (rho_vector == NULL){
     printf("Error!\n");
+  }else{
+    for (int i = 0; i < n; i++) {
+      printf("%.2f ", rho_vector[i]);
+    }
   }
 
   //free matrix
@@ -46,11 +58,12 @@ int main(int argc, char *argv[]){
   return EXIT_SUCCESS;
 }
 
-int * pearson_cor(int ** matrix, int * vector, int n){
-  int * rho_vector = NULL;
-  int x, x_sq, y, y_sq, xy; 
+double * pearson_cor(int ** matrix, int * vector, int n){
+  double * rho_vector = NULL;
+  int x, x_sq, y, y_sq, xy;
+  double numerator, denominator;
 
-  rho_vector = init_empty_vector(n);
+  rho_vector = (double *) malloc(n * sizeof(double));
   if(vector == NULL)
       return NULL;
 
@@ -62,13 +75,9 @@ int * pearson_cor(int ** matrix, int * vector, int n){
     x_sq = get_vector_sum_sq(vector_col_i, n);
     y_sq = get_vector_sum_sq(vector, n);
     xy = get_sum_of_products(vector_col_i, vector, n);
-
-    /* TODO: Write the code snippet that finds the Pearson Correlation
-    * Coefficient of two vectors. 
-    * Guide: rho_numerator = (n * summ(Xcoly * vecy) - (summX * summY)) 
-    * rho_denominator = sqrt((n * sum(X^2) - summX^2) * (n * sum(Y^2) - summY^2)) 
-    * rho = rho_numerator / rho_denominator
-    */
+    numerator = (double) (n * xy) - (y*x);
+    denominator = sqrt(((n*x_sq) - (x*x)) * ((n*y_sq) * (y*y)));
+    rho_vector[i] = numerator / denominator;
 
     free(vector_col_i);
   }
