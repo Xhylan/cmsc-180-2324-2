6273a674aec77800b842fe9753a5715a91fcb7c8
Xhylan <mbdollentes@up.edu.ph>
2024-05-06 07:49:43 +0000

    Added send() code

--- c/lab04.c
+++ c/lab04.c
@@ -6,11 +6,12 @@
 #include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
-
 #include "lab04.h"
 
 #define MASTER 0
 #define SLAVE 1
+#define TRUE 10
+#define FALSE 11
 
 typedef struct ADDR_COLL{
     char * IP_addr;
@@ -18,46 +19,75 @@ typedef struct ADDR_COLL{
     struct ADDR_COLL * next;
 }net_address;
 
+net_address * head = NULL;
+
+int is_address_found(char* IP_addr, int port){
+    net_address *temp = head;
+
+    while(temp != NULL){
+        if(strcmp(IP_addr, head -> IP_addr) == 0){
+            if(temp -> port == port) return TRUE;
+        } 
+
+        temp = temp -> next;
+    }
+    
+    return FALSE;
+}
+
+void send_over_socket();
+void send_over_socket_threaded();
+void send_over_socket_thread_affine();
+
 int main(int argc, char *argv[]){
-    int n, p, s, sockfd;
-    int ** matrix, * vector;
+    int n, p, s, sockfd, opt = 1;
+    int ** matrix, * vector, ***submatrices;
     double * rho_vector, elapsed;
     struct sockaddr_in server, client;
     struct timespec start, end;
     socklen_t client_addr_size;
     FILE * fd;
 
-    printf("N: ");
+    printf("[o] N: ");
     scanf("%d", &n);
     getchar();
 
-    printf("Port: ");
+    printf("[o] Port: ");
     scanf("%d", &p);
     getchar();
 
-    printf("Status (0 - Master, 1 - Slave): ");
+    printf("[o] Status (0 - Master, 1 - Slave): ");
     scanf("%d", &s);
     getchar();
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd < 0){
-        perror("socket()");
+        perror("[-] Error creating socket ");
         return EXIT_FAILURE;
     }
 
+    //Setting socket options.
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))){
+        perror("[-] Failed to set socket options: ");
+        exit(EXIT_FAILURE);
+    }
+
     if (s == MASTER) {
-        int expected_clients = 0, clients_connected = 0;
-        net_address * head = NULL;
+        int expected_clients = 0, clients_connected = 0, size;
+
+        printf("[o] Expected connections: ");
+        scanf("%d", &expected_clients);
+        getchar();
 
         srand(time(NULL));
         matrix = initialize_matrix(n);
         vector = initialize_vector(n);
-        rho_vector = initialize_rho_vector(n);
+        submatrices = divide_into_submatrices(matrix, n, expected_clients);
 
         check_if_file_exists(&fd, "clients.cfg");
 
         if(fd == NULL){
-            printf("Can't find client configuration file!\n");
+            printf("[-] Can't find client configuration file!\n");
             return EXIT_FAILURE;
         }
 
@@ -69,25 +99,19 @@ int main(int argc, char *argv[]){
 
             token = strtok(netAddressString, ":");
             newAddress -> IP_addr = token;
-            printf("%s ", token);
             token = strtok(NULL, ":");
             newAddress -> port = atoi(token);
-            printf("%s ", token);
             newAddress -> next = NULL;
             
             if (head != NULL) {
-                printf("not empty!\n");
                 net_address * temp = head;
                 while (temp -> next != NULL) temp = temp -> next;
                 temp -> next = newAddress;
             }
 
             if (head == NULL) {
-                printf("Empty!\n");
                 head = newAddress;
             }
-
-            expected_clients++;
         }
 
         server.sin_family = AF_INET;
@@ -104,9 +128,10 @@ int main(int argc, char *argv[]){
             return 1;
         }
 
-        printf("Server listening on port %d\n", ntohs(server.sin_port));
+        printf("[+] Server listening on port %d\n", ntohs(server.sin_port));
     
         while (clients_connected < expected_clients) {
+            int bytes_to_send, sent_bytes, size;
             int new_socket = accept(sockfd, (struct sockaddr*) &client, &client_addr_size);
             struct sockaddr_in client;
             socklen_t client_addr_size = sizeof(client);
@@ -120,23 +145,43 @@ int main(int argc, char *argv[]){
 
             char client_IP[INET_ADDRSTRLEN]; 
             inet_ntop(AF_INET, &(client.sin_addr), client_IP, client_addr_size);
-
             int client_port = ntohs(client.sin_port);
 
             printf("Client connected from: %s:%d\n", client_IP, client_port);
+
+            if(is_address_found(client_IP, client_port) == FALSE){
+                printf("[-] The server cannot accept connections from this address.\n ");
+                close(new_socket);
+                continue;
+            }
+
+            size = n/expected_clients;
+            bytes_to_send = htonl(size);
+            sent_bytes = send(new_socket, &bytes_to_send, sizeof(bytes_to_send), 0);
+            if(sent_bytes != sizeof(bytes_to_send)){
+                perror("[-] Failed to send data: ");
+                exit(EXIT_FAILURE);
+            }
+
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < size; j++){
+                    bytes_to_send = htonl(submatrices[clients_connected][i][j]);
+                    sent_bytes = send(new_socket, &bytes_to_send, sizeof(bytes_to_send), 0);
+                    if(sent_bytes != sizeof(bytes_to_send)){
+                        perror("[-] Failed to send data: ");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+            }
+
             clients_connected++;
         }
-
-        /*
-            *   TODO: Slice and distribute matrix to clients connecting.
-            *   One to many broadcast, log(p) implementation
-            *
-        */
-       
-       int *** submatrices = divide_into_submatrices(matrix, n, clients_connected);
     }
 
     if (s == SLAVE){
+        int bytes_to_recv, recv_bytes, cols;
+        int **submatrix;
+
         check_if_file_exists(&fd, "server.cfg");
         struct sockaddr_in client_addr;
 
@@ -158,18 +203,44 @@ int main(int argc, char *argv[]){
             perror("connect()");
             return 1;
         }
+        
+        printf("[+] Connected\n");
 
-        /*
-            *
-            *   TODO: Distribute matrix to clients.
-            *   Use server.cfg for the network details.
-            *   One to one broadcast, log(p) implementation
-            * 
-        */
+        recv_bytes = recv(sockfd, &bytes_to_recv, sizeof(bytes_to_recv), 0);
+        if(recv_bytes != sizeof(bytes_to_recv)){
+            perror("[-] Failed to receive data: ");
+            exit(EXIT_FAILURE);
+        }
+        cols = ntohl(bytes_to_recv);
+    
+        printf("[+] Received: %d", cols); 
 
-        printf("Connected");
+        submatrix = init_empty_matrix(n, cols);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < cols; j++){
+                recv_bytes = recv(sockfd, &bytes_to_recv, sizeof(bytes_to_recv), 0);
+                if(recv_bytes != sizeof(bytes_to_recv)){
+                    perror("[-] Failed to receive data: ");
+                    exit(EXIT_FAILURE);
+                }
+                submatrix[i][j] = ntohl(bytes_to_recv);
+            }
+        }
+
     }
 
     close(sockfd);
     return EXIT_SUCCESS;
+}
+
+void send_over_socket(){
+
+}
+
+void send_over_socket_threaded(){
+
+}
+
+void send_over_socket_thread_affine(){
+    
 }
